e617dbd101db271c17678af91415e70362a7a0ef
Lide8 <lide.gonzalez@opendeusto.es>
2025-11-05 11:45:45 +0100

    correccion models.py

--- traveloffers/appTravel/models.py
+++ traveloffers/appTravel/models.py
@@ -32,16 +32,6 @@ class Oferta(models.Model):
     def __str__(self):
         return self.titulo
 
-from django.db import models
-
-class Imagen(models.Model):
-    titulo = models.CharField(max_length=100)
-    imagen = models.ImageField(upload_to='imagenes/')
-
-    def __str__(self):
-        return self.titulo
-    
-from django.db import models
 
 class Imagen(models.Model):
     titulo = models.CharField(max_length=100)

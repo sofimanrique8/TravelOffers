d440cef79d68f1fc465a493c9840dd90023ce75a
Lide8 <lide.gonzalez@opendeusto.es>
2025-11-07 11:43:56 +0100

    iconos de países + relación

--- traveloffers/appTravel/models.py
+++ traveloffers/appTravel/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Pais(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
+    icono = models.ImageField(upload_to='iconos_paises/', blank=True, null=True)
     class Meta:
         verbose_name_plural = "Países"
     def __str__(self): return self.nombre

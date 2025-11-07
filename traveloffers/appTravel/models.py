3ebef6d60162cb44c3e91a672fbcb4ced95200ed
Lide8 <lide.gonzalez@opendeusto.es>
2025-11-07 11:03:38 +0100

    implementación de imagenes

--- traveloffers/appTravel/models.py
+++ traveloffers/appTravel/models.py
@@ -10,6 +10,7 @@ class Pais(models.Model):
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
+    icono = models.ImageField(upload_to='iconos_categorias/', blank=True, null=True)
     def __str__(self): return self.nombre
 
 class Oferta(models.Model):

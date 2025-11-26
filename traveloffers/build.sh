59121835ca085746444aa1363f1afdda1a19afc9
Ana Urrutia <ana.urrutia.f@opendeusto.es>
2025-11-26 16:29:26 +0100

    Render (paso4, crear usuario)

--- traveloffers/build.sh
+++ traveloffers/build.sh
@@ -5,11 +5,12 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --no-input
-
 # Apply database migrations
-python manage.py migrate
+python manage.py migrate --noinput
 
 # Cargar datos iniciales
 python manage.py loaddata datos_utf8.json || true
+python manage.py loaddata user_render_ana.json || true
+
+# Collect static files
+python manage.py collectstatic --no-input
